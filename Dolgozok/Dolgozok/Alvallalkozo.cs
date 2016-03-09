f290d3f7f253f8170a5bff98c0170f5bbd0eb6f7
Farkas János <jancsiij@gmail.com>
2016-03-09 13:54:37 +0100

    missing file

---
+++ Dolgozok/Dolgozok/Alvallalkozo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozok
+{
+     class Alvallalkozo : Dolgozo, IComparable
+    {
+        public DateTime SzerzodesVege { get; private set; }
+        public string Feladat { get; private set; }
+        public int SikerDij { get; private set; }
+
+        public Alvallalkozo(string nev, string cim, int adoSzam, float alapFizetes, DateTime szerzodesVege, string feladat, int sikerDij) :
+            base(nev, cim, adoSzam, alapFizetes)
+        {
+            this.SzerzodesVege = szerzodesVege;
+            this.Feladat = feladat;
+            this.SikerDij = sikerDij;
+        }
+
+        public override float Fizetes()
+        {
+            return base.Fizetes() + SikerDij;
+        }
+
+        public new string Print()
+        {
+            return SzerzodesVege.ToShortDateString() + ", " + Nev;
+        }
+
+        public int CompareTo(object obj)
+        {
+            
+            Alvallalkozo other = obj as Alvallalkozo;
+
+            if (SzerzodesVege >= other.SzerzodesVege)
+            {
+                return Nev.CompareTo(other.Nev);
+            }
+            return -1;
+        }
+    }
+}
